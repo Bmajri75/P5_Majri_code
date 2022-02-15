8cc5fcec8868505dd824259094aff4ab1084f2a4
Bechir Majris <bmajri@gmail.com>
2022-02-15 22:17:33 +0100

    en PLS

--- front/js/cart.js
+++ front/js/cart.js
@@ -7,20 +7,36 @@ fetch(`http://localhost:3000/api/products/`)
     }
   })// ce dernier me renvoie encore une promise j'utilise encore then pour les recuperer
   .then(data => {
+
     //     EN amont cree un array dans product avec ce qu'il retourne du localstorage 
     // il faut recupere les doneee du localStorage
     // remplacer le prix et les id les couleurs par ce qu'il y'a dans le panier
+    console.log(data)
+    // calcule du prix total  
+    if (localStorageReturn) {
+      let idArray = [];
+      localStorageReturn.forEach(kanap => {
+        // je recupere les id du localstorage 
+        idArray.push(kanap.id);
+        for (let i = 0; i < data.length; i++) {
+          if (data[i]._id === idArray[i]) {
+            return prix = data[i].price;
+          }
+        }
+      });
+    }
+
     for (let i = 0; i < localStorageReturn.length; i++) {
 
       document.querySelector('#cart__items').insertAdjacentHTML('afterbegin', ` <article article class="cart__item" data - id="${localStorageReturn[i].id}" data - color="${localStorageReturn[i].color}" >
-    <div class="cart__item__img">
+      <div class="cart__item__img">
       <img src="${localStorageReturn[i].image}" alt="Photographie d'un canapé">
-    </div>
-    <div class="cart__item__content">
+      </div>
+      <div class="cart__item__content">
       <div class="cart__item__content__description">
       <h2>${localStorageReturn[i].name}</h2 >
       <p>${localStorageReturn[i].color}</p>
-      <p>${data.price}€</p>
+      <p>${prix}€</p>
       </div>
       <div class="cart__item__content__settings">
         <div class="cart__item__content__settings__quantity">
